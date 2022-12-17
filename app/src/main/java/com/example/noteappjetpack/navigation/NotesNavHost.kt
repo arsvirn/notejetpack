9a77bcfda0c5b0e28a410e0e45e4199bffc2b46f
arsvirn <arsvirn@gmail.com>
2022-12-17 22:04:40 +0300

    Lesson-2

--- app/src/main/java/com/example/noteappjetpack/navigation/NotesNavHost.kt
+++ app/src/main/java/com/example/noteappjetpack/navigation/NotesNavHost.kt
@@ -5,26 +5,23 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.noteappjetpack.screens.Add
-import com.example.noteappjetpack.screens.Main
-import com.example.noteappjetpack.screens.Note
-import com.example.noteappjetpack.screens.Start
+import com.example.noteappjetpack.screens.*
 
-sealed class NavRoutes(val route: String) {
-    object Start: NavRoutes("start_screen")
-    object Main: NavRoutes("main_screen")
-    object Add: NavRoutes("add_screen")
-    object Note: NavRoutes("note_screen")
+sealed class NavRoute(val route: String) {
+    object Start: NavRoute("start_screen")
+    object Main: NavRoute("main_screen")
+    object Add: NavRoute("add_screen")
+    object Note: NavRoute("note_screen")
 }
 
 @Composable
 fun NotesNavHost() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = NavRoutes.Start.route) {
-        composable(NavRoutes.Start.route) { Start(navController = navController) }
-        composable(NavRoutes.Main.route) { Main(navController = navController) }
-        composable(NavRoutes.Add.route) { Add(navController = navController) }
-        composable(NavRoutes.Note.route) { Note(navController = navController) }
+    NavHost(navController = navController, startDestination = NavRoute.Start.route) {
+        composable(NavRoute.Start.route) { StartScreen(navController = navController) }
+        composable(NavRoute.Main.route) { MainScreen(navController = navController) }
+        composable(NavRoute.Add.route) { AddScreen(navController = navController) }
+        composable(NavRoute.Note.route) { NoteScreen(navController = navController) }
     }
 }
